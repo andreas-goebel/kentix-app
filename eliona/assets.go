f0283f3238c39ddac47cba6f9a4f57257700d9c6
Zdenek Devaty <zdenek.devaty@eliona.io>
2023-03-09 19:15:04 +0100

    refactor asset creation

--- eliona/assets.go
+++ eliona/assets.go
@@ -28,67 +28,31 @@ import (
 	"github.com/eliona-smart-building-assistant/go-utils/log"
 )
 
-// CreateAssetsIfNecessary create all assets for specification if not already exists
 func CreateAssetsIfNecessary(config apiserver.Configuration, spec kentix.DeviceInfo) error {
 	for _, projectId := range conf.ProjIds(config) {
-		if err := createAssetIfNecessary(config, projectId, nil, spec); err != nil {
+		if err := createDeviceAssetIfNecessary(config, projectId, spec); err != nil {
 			return fmt.Errorf("creating assets for device %s: %v", spec.Serial, err)
 		}
 	}
 	return nil
 }
 
-// createAssetIfNecessary create asset for specification if not already exists
-func createAssetIfNecessary(config apiserver.Configuration, projectId string, parentAssetId *int32, spec kentix.DeviceInfo) error {
-	// Get known asset id from configuration
-	assetID, err := conf.GetAssetId(context.Background(), config, projectId, spec.Serial)
-	if err != nil {
-		return fmt.Errorf("finding asset ID: %v", err)
+func createDeviceAssetIfNecessary(config apiserver.Configuration, projectId string, spec kentix.DeviceInfo) error {
+	assetData := assetData{
+		config:        config,
+		projectId:     projectId,
+		parentAssetId: nil,
+		identifier:    spec.Serial,
+		assetType:     spec.AssetType,
+		name:          fmt.Sprintf("%s (%s)", spec.Name, spec.IPAddress),
+		description:   fmt.Sprintf("%s (%s)", spec.Name, spec.Serial),
 	}
-	if assetID != nil {
-		return nil
-	}
-
-	// If no asset exists for project and configuration, create a new one
-	name := name(spec)
-	description := description(spec)
-
-	newId, err := asset.UpsertAsset(api.Asset{
-		ProjectId:               projectId,
-		GlobalAssetIdentifier:   spec.Serial,
-		Name:                    *api.NewNullableString(common.Ptr(name)),
-		AssetType:               spec.AssetType,
-		Description:             *api.NewNullableString(common.Ptr(description)),
-		ParentLocationalAssetId: *api.NewNullableInt32(parentAssetId),
-	})
-	if err != nil {
-		return fmt.Errorf("upserting asset into Eliona: %v", err)
-	}
-	if newId == nil {
-		return fmt.Errorf("cannot create asset: %s", name)
-	}
-
-	// Remember the asset id for further usage
-	if err := conf.InsertAsset(context.Background(), config, projectId, spec.Serial, *newId); err != nil {
-		return fmt.Errorf("inserting asset to config db: %v", err)
-	}
-
-	log.Debug("eliona", "Created new asset for project %s and device %s.", projectId, spec.Serial)
-
-	return nil
+	return createAssetIfNecessary(assetData)
 }
 
-func name(specification kentix.DeviceInfo) string {
-	return fmt.Sprintf("%s (%s)", specification.Name, specification.IPAddress)
-}
-
-func description(specification kentix.DeviceInfo) string {
-	return fmt.Sprintf("%s (%s)", specification.Name, specification.Serial)
-}
-
-// CreateDoorlockAssetsIfNecessary create all assets for specification if not already exists
 func CreateDoorlockAssetsIfNecessary(config apiserver.Configuration, spec kentix.DoorLock) error {
 	for _, projectId := range conf.ProjIds(config) {
+		// TODO: Implement parent asset ID for doorlocks?
 		if err := createDoorlockAssetIfNecessary(config, projectId, nil, spec); err != nil {
 			return fmt.Errorf("creating assets for device %s: %v", spec.Serial, err)
 		}
@@ -96,10 +60,32 @@ func CreateDoorlockAssetsIfNecessary(config apiserver.Configuration, spec kentix
 	return nil
 }
 
-// createDoorlockAssetIfNecessary create asset for specification if not already exists
 func createDoorlockAssetIfNecessary(config apiserver.Configuration, projectId string, parentAssetId *int32, spec kentix.DoorLock) error {
+	assetData := assetData{
+		config:        config,
+		projectId:     projectId,
+		parentAssetId: parentAssetId,
+		identifier:    spec.Serial,
+		assetType:     kentix.DoorlockAssetType,
+		name:          fmt.Sprintf("%s (%s)", spec.Name, spec.Address),
+		description:   fmt.Sprintf("%s (%s)", spec.Name, spec.Serial),
+	}
+	return createAssetIfNecessary(assetData)
+}
+
+type assetData struct {
+	config        apiserver.Configuration
+	projectId     string
+	parentAssetId *int32
+	identifier    string
+	assetType     string
+	name          string
+	description   string
+}
+
+func createAssetIfNecessary(d assetData) error {
 	// Get known asset id from configuration
-	assetID, err := conf.GetAssetId(context.Background(), config, projectId, spec.Serial)
+	assetID, err := conf.GetAssetId(context.Background(), d.config, d.projectId, d.identifier)
 	if err != nil {
 		return fmt.Errorf("finding asset ID: %v", err)
 	}
@@ -107,39 +93,27 @@ func createDoorlockAssetIfNecessary(config apiserver.Configuration, projectId st
 		return nil
 	}
 
-	// If no asset exists for project and configuration, create a new one
-	name := doorlockName(spec)
-	description := doorlockDescription(spec)
-
 	newId, err := asset.UpsertAsset(api.Asset{
-		ProjectId:               projectId,
-		GlobalAssetIdentifier:   spec.Serial,
-		Name:                    *api.NewNullableString(common.Ptr(name)),
-		AssetType:               kentix.DoorlockAssetType,
-		Description:             *api.NewNullableString(common.Ptr(description)),
-		ParentLocationalAssetId: *api.NewNullableInt32(parentAssetId),
+		ProjectId:               d.projectId,
+		GlobalAssetIdentifier:   d.identifier,
+		Name:                    *api.NewNullableString(common.Ptr(d.name)),
+		AssetType:               d.assetType,
+		Description:             *api.NewNullableString(common.Ptr(d.description)),
+		ParentLocationalAssetId: *api.NewNullableInt32(d.parentAssetId),
 	})
 	if err != nil {
 		return fmt.Errorf("upserting asset into Eliona: %v", err)
 	}
 	if newId == nil {
-		return fmt.Errorf("cannot create asset: %s", name)
+		return fmt.Errorf("cannot create asset %s", d.name)
 	}
 
 	// Remember the asset id for further usage
-	if err := conf.InsertAsset(context.Background(), config, projectId, spec.Serial, *newId); err != nil {
+	if err := conf.InsertAsset(context.Background(), d.config, d.projectId, d.identifier, *newId); err != nil {
 		return fmt.Errorf("inserting asset to config db: %v", err)
 	}
 
-	log.Debug("eliona", "Created new asset for project %s and doorlock %s.", projectId, spec.Serial)
+	log.Debug("eliona", "Created new asset for project %s and device %s.", d.projectId, d.identifier)
 
 	return nil
-}
-
-func doorlockName(specification kentix.DoorLock) string {
-	return fmt.Sprintf("%s (%s)", specification.Name, specification.Address)
-}
-
-func doorlockDescription(specification kentix.DoorLock) string {
-	return fmt.Sprintf("%s (%s)", specification.Name, specification.Serial)
 }
